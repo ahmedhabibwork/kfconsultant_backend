c32fecbd60b41d37f73d5ba077448db9eb437032
ahmedhabibwork <ahmedhabibwork80@gmail.com>
2025-09-27 20:53:45 +0300

    Finsh

--- app/Enums/PagesEnum.php
+++ app/Enums/PagesEnum.php
@@ -8,13 +8,10 @@ enum PagesEnum: string
     case Blogs = 'blogs';
     case AboutUs = 'about_us';
     case ContactUs = 'contact_us';
-    case SubCategory = 'sub_category';
     case Trips = 'trips';
-    case TouristTrips = 'tourist_trips';
-    case HajjUmrah = 'hajj_umrah';
-    case Flights = 'flights';
-    case Transport = 'transport';
-    case Hotels = 'hotels';
+    case FAQ = 'faq';
+    
+
 
     public function label(): string
     {
@@ -23,13 +20,8 @@ enum PagesEnum: string
             self::Blogs => __('Blogs'),
             self::AboutUs => __('About Us'),
             self::ContactUs => __('Contact Us'),
-            self::SubCategory => __('Sub Category'),
             self::Trips => __('Trips'),
-            self::TouristTrips => __('رحلات سياحية'),
-            self::HajjUmrah => __('حج وعمرة'),
-            self::Flights => __('تذاكر الطيران'),
-            self::Transport => __('نقل سياحي'),
-            self::Hotels => __('حجز فنادق'),
+            self::FAQ => __('FAQ'),
         };
     }
 }
