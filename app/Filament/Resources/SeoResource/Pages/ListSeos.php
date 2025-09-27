b61b6a1aaa102e6b4aa91f4a780259bd9b24dfb5
ahmedhabibwork <ahmedhabibwork80@gmail.com>
2025-09-27 20:50:29 +0300

    Finsh

--- app/Filament/Resources/SeoResource/Pages/ListSeos.php
+++ app/Filament/Resources/SeoResource/Pages/ListSeos.php
@@ -13,7 +13,7 @@ class ListSeos extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-        //    Actions\CreateAction::make(),
+            Actions\CreateAction::make(),
         ];
     }
 }
