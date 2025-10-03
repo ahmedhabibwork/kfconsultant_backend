9c6e6888f9624c731bfe1d12b3f2e5285b6951f6
ahmedhabibwork <ahmedhabibwork80@gmail.com>
2025-10-03 15:42:19 +0300

    Finish

--- app/Filament/Resources/TripResource.php
+++ app/Filament/Resources/TripResource.php
@@ -33,6 +33,7 @@ class TripResource extends Resource
     {
         return null;
     }
+    protected static ?int $navigationSort = -99;
 
     // أو لو عايزه في جروب لكن الجروب نفسه يطلع الأول
     public static function getNavigationGroupSort(): ?int
