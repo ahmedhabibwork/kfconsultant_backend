314196e6a7a3382994b2926c93d033d4b01fe30b
ahmedhabibwork <ahmedhabibwork80@gmail.com>
2025-10-03 15:36:20 +0300

    Finish

--- app/Filament/Resources/TripResource.php
+++ app/Filament/Resources/TripResource.php
@@ -29,7 +29,7 @@ class TripResource extends Resource
 {
     protected static ?string $model = Trip::class;
 
-    protected static ?int $navigationSort = 1;
+    protected static ?int $navigationSort = -1;
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     public static function getNavigationGroup(): ?string
     {
