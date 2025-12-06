0c0630eb0fb19f3a255f098223aaba029b2434a3
ahmedhabibwork <ahmedhabibwork80@gmail.com>
2025-12-06 19:34:58 +0200

    f

--- app/Filament/Resources/ContactUsResource.php
+++ app/Filament/Resources/ContactUsResource.php
@@ -71,11 +71,11 @@ class ContactUsResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    //  Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }
